65f2bb90897a9a2a90f6a97bf99d973f3f8adea2
tomascrotti <tomascrotti02@gmail.com>
2024-12-15 15:07:46 -0300

    java spring
    archivos básicos

---
+++ src/main/java/com/fulvo/backend/FulvoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fulvo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulvoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulvoBackendApplication.class, args);
+	}
+
+}
